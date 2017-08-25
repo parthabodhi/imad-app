324e992c1c4d77e28f46614f81a59fc6463eaf95
parthabodhi <danielcops1996@gmail.com>
2017-08-25 13:28:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article_one', function (req, res) {
+  res.sendFile("Articles_one will be served as requested");
+});
+
+app.get('/article_one', function (req, res) {
+  res.sendFile("Articles_two will be served as requested");
+});
+
+app.get('/article_one', function (req, res) {
+  res.sendFile("Articles_three will be served as requested");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
